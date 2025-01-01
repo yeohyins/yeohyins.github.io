880cc2fef4ac4c7d178541bd1d4a1cc565fad206
yins <yins233062@dasein.edu.my>
2025-01-01 21:29:44 +0800

    set javascript for mercury page

--- web/js/mercury.js
+++ web/js/mercury.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const section = document.querySelector('section'); // Target the <section> element
-    const [firstDiv, secondDiv] = section.children; // Destructure to get the child <div> elements
+    const section = document.getElementById('background-section');
 
     section.addEventListener('mouseenter', () => {
-        secondDiv.style.opacity = 1; // Show the second background on hover
-        secondDiv.style.zIndex = 2; // Bring it to the front
+        section.style.backgroundImage = "url(img/mercury-internal.png)"; // Change to hover background image
     });
 
     section.addEventListener('mouseleave', () => {
-        secondDiv.style.opacity = 0; // Hide the second background
-        secondDiv.style.zIndex = 0; // Send it to the back
+        section.style.backgroundImage = "url(img/mercury-background.png)"; // Revert to default background image
     });
 });
