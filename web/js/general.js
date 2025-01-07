abe5744431d06d4bd1557ae76e80be8be3ea3bec
beilie <beilie233158@dasein.edu.my>
2025-01-07 11:41:56 +0800

    add javascript to back to top button

--- web/js/general.js
+++ web/js/general.js
@@ -1,3 +1,16 @@
+let mybutton = document.getElementById("backButton");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
 function myFunction(x) {
     x.classList.toggle("change");
 }
@@ -19,4 +32,13 @@ for (i = 0; i < acc.length; i++) {
             panel.style.display = "block";
         }
     });
+}
+
+// Get the button:
+
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
