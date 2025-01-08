ec9ec42dfd1b7ab6452ac107bbaf8daab689c30e
YAP BEI LIE <beilie233158@dasein.edu.my>
2025-01-08 20:03:54 +0800

    fixed hamburger menu

--- web/js/general.js
+++ web/js/general.js
@@ -1,3 +1,27 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "flex") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "flex";;
+    }
+  });
+
+}
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
 let mybutton = document.getElementById("backButton");
 
 // When the user scrolls down 20px from the top of the document, show the button
@@ -6,36 +30,11 @@ window.onscroll = function () { scrollFunction() };
 function scrollFunction() {
     if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
         mybutton.style.display = "block";
+        console.log("Show back button")
     } else {
         mybutton.style.display = "none";
     }
 }
-
-function myFunction(x) {
-    x.classList.toggle("change");
-}
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
-        this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
-
-// Get the button:
-
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
